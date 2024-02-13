c20ae3b8203848b1f382a820f5de455e76631ecc
Sajarin M <sajarinm@gmail.com>
2024-02-13 12:16:44 +0530

    initial version

---
+++ prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 100,
+  bracketSameLine: false,
+  useTabs: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
